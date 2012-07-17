b9a6d936f859f1e5d7bfb77964da582c8c0e2f14
Aaron Unger <aaron.unger@gmail.com>
2012-07-17 12:11:43 -0700

    Investigation needs publication status

--- spec/dummy/app/models/investigation.rb
+++ spec/dummy/app/models/investigation.rb
@@ -1,5 +1,5 @@
 class Investigation < ActiveRecord::Base
-  attr_accessible :activities, :user, :name
+  attr_accessible :activities, :user, :name, :publication_status
 
   belongs_to :user
   has_many :activities
--- spec/dummy/db/migrate/20120717180840_create_investigations.rb
+++ spec/dummy/db/migrate/20120717180840_create_investigations.rb
@@ -2,6 +2,7 @@ class CreateInvestigations < ActiveRecord::Migration
   def change
     create_table :investigations do |t|
       t.string :name
+      t.string :publication_status
       t.integer :user_id
       t.timestamps
     end
